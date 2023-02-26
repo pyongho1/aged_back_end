2195becc8761657bdd8102c00fd7f15c62a961f6
Yong Park <pyongho0306@gmail.com>
2023-02-26 14:10:31 -0800

    stub up create, index, and update

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,38 @@
 const { Post } = require("../models");
 
+const create = async (req, res) => {
+  try {
+    req.body.profileId = req.user.profile.id;
+    const post = await Post.create(req.body);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
+};
+
+const index = async (req, res) => {
+  try {
+    const posts = await Post.findAll();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    req.body.profileId = req.user.profile.id;
+    const post = await Post.findByPk(req.params.id);
+    post.set(req.body);
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
+};
+
 module.exports = {
-  
-}
+  create,
+  index,
+  update,
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
-const postsCtrl = require("../controllers/post.js");
+const postsCtrl = require("../controllers/posts.js");
+const middleware = require("../middleware/auth.js");
+
+const { decodeUserFromToken, checkAuth } = middleware;
 
 /*---------- Public Routes ----------*/
 
 /*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken);
+router.get("/", checkAuth, postsCtrl.index);
+router.post("/", checkAuth, postsCtrl.create);
+router.put("/:id", checkAuth, postsCtrl.update);
 
 module.exports = router;
